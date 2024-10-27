93056b9d5a3baa8b8c69d18fd0bb89c9aa7bfbf8
VadimSel <selyaaa33@gmail.com>
2024-10-27 11:26:01 +0300

    Added key input

--- src/components/previewPage.tsx
+++ src/components/previewPage.tsx
@@ -4,23 +4,30 @@ import { MovieResponse } from "./types";
 import { useNavigate } from "react-router-dom";
 
 type PreviewPageProps = {
-  setSelectedMovie: (movie: MovieResponse) => void
-}
+	setSelectedMovie: (movie: MovieResponse) => void;
+};
 
 export const PreviewPage = ({ setSelectedMovie }: PreviewPageProps) => {
 	const [searchValue, setSearchValue] = useState("");
 	const [searchResValue, setSearchResValue] = useState<MovieResponse[]>([]);
 	const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false)
 
-  const navigate = useNavigate()
+	const navigate = useNavigate();
 
 	const inputHandler = async (event: ChangeEvent<HTMLInputElement>) => {
 		setSearchValue(event.target.value);
 	};
 
 	const searchSubmit = async () => {
+    if (!searchValue.trim()) {
+      setError(true) 
+      return
+    }
+
 		const res = await getMovies(searchValue);
 		if (res) {
+      setError(false)
 			setIsLoading(true);
 			setSearchResValue(res);
 			setIsLoading(false);
@@ -29,15 +36,25 @@ export const PreviewPage = ({ setSelectedMovie }: PreviewPageProps) => {
 
 	const selectedMovieHandler = (movie: MovieResponse) => {
 		setSelectedMovie(movie);
-    navigate('/movie')
+		navigate("/movie");
 	};
 
 	console.log(searchValue);
 
 	return (
 		<div>
-			<input type="text" value={searchValue} onChange={inputHandler} />
+			<input
+				type="text"
+				value={searchValue}
+				onChange={inputHandler}
+				onKeyDown={(e) => {
+					if (e.key === "Enter") {
+						searchSubmit();
+					}
+				}}
+			/>
 			<button onClick={searchSubmit}>Найти фильм</button>
+      {error && 'Введите название фильма'}
 			{isLoading && <p>Идёт загрузка</p>}
 			{searchResValue.map((movie) => {
 				return (
@@ -53,4 +70,4 @@ export const PreviewPage = ({ setSelectedMovie }: PreviewPageProps) => {
 			})}
 		</div>
 	);
-}
+};
