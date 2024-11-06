87b616b8023c33c1d62586b413dab4a998fc63bd
VadimSel <selyaaa33@gmail.com>
2024-11-06 22:00:55 +0300

    add style

--- src/components/fullPage.tsx
+++ src/components/fullPage.tsx
@@ -1,4 +1,5 @@
 import { MovieResponse } from "./types";
+import s from "./fullPage.module.css";
 
 // Варианты
 // www.kinopoisk.gg
@@ -14,30 +15,36 @@ export const FullPage = ({ movie }: InfoProps) => {
 		return null;
 	}
 	return (
-		<div>
-			<img src={movie.poster.previewUrl} alt="poster" />
-			<p>
-				Название: {movie.name} ({movie.alternativeName})
-			</p>
-			<p>Год: {movie.year}</p>
-			<p>Жанры: {movie.genres.map((genre) => genre.name).join(", ")}</p>
-			<p>
-				{movie.isSeries ? (
-					<p>Продолжительность одной серии: {movie.seriesLength % 60} мин</p>
-				) : (
-					`Продолжительность: ${Math.floor(movie.movieLength / 60)} ч ${
-						movie.movieLength % 60
-					} мин`
-				)}
-			</p>
-			<p>Описание: {movie.description}</p>
-			<a
-				href={`https://www.kinopoisk.gg/film/${movie.id}`}
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				Смотреть
-			</a>
+		<div className={s.container}>
+			<div className={s.fullPageContainer}>
+				<div className={s.imageContainer}>
+					<img src={movie.poster.previewUrl} alt="poster" />
+				</div>
+				<div className={s.fullPageInfo}>
+					<p>
+						Название: {movie.name} ({movie.alternativeName})
+					</p>
+					<p>Год: {movie.year}</p>
+					<p>Жанры: {movie.genres.map((genre) => genre.name).join(", ")}</p>
+					<p>
+						{movie.isSeries ? (
+							<p>Продолжительность одной серии: {movie.seriesLength % 60} мин</p>
+						) : (
+							`Продолжительность: ${Math.floor(movie.movieLength / 60)} ч ${
+								movie.movieLength % 60
+							} мин`
+						)}
+					</p>
+					<p>Описание: {movie.description}</p>
+					<a
+						href={`https://www.kinopoisk.gg/film/${movie.id}`}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Смотреть
+					</a>
+				</div>
+			</div>
 		</div>
 	);
 };
